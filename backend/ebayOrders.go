610640a1e07423afaf508092424b48fff47afff9
Jacob Newman <jnewman@thoughtworks.com>
2018-09-02 16:25:50 +0100

    Extracts ebayOrders into seperate file

--- backend/ebayOrders.go
+++ backend/ebayOrders.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"time"
 )
 
@@ -53,33 +51,6 @@ type EbayOrders struct {
 	} `json:"orders"`
 }
 
-type Orders struct {
-	Orders []Order `json:"orders"`
-}
-
-type Order struct {
-	OrderItems      []OrderItem     `json:"orderItems"`
-	OrderDate       string          `json:"orderDate"`
-	OrderTotal      string          `json:"orderTotal"`
-	ShipByDate      string          `json:"shipByDate"`
-	ShippingAddress ShippingAddress `json:"shippingAddress"`
-	Platform        string          `json:"platform"`
-}
-
-type OrderItem struct {
-	ItemName  string `json:"itemName"`
-	ImageUrl  string `json:"imageUrl"`
-	ItemPrice string `json:"itemPrice"`
-}
-
-type ShippingAddress struct {
-	Name        string `json:"name"`
-	AddressLine string `json:"addressLine"`
-	City        string `json:"city"`
-	PostCode    string `json:"postCode"`
-	Country     string `json:"country"`
-}
-
 var mockedEbayOrders = Orders{Orders: []Order{
 	{OrderItems: []OrderItem{
 		{
@@ -116,24 +87,7 @@ var mockedEbayOrders = Orders{Orders: []Order{
 			Country:     "UK",
 		},
 		Platform: "etsy",
-	}},
-}
-
-func main() {
-	http.HandleFunc("/get-orders", GetOrders)
-	log.Fatal(http.ListenAndServe(":3001", nil))
-}
-
-func GetOrders(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-	w.Header().Set("content-type", "application/json")
-	//ebayOrders := fetchEbayOrders()
-	//ebayItemImage := fetchEbayItemImage("273258129797")xa
-
-	fmt.Println("MOCKED ORDERS: ", mockedEbayOrders)
-	orders, _ := json.Marshal(mockedEbayOrders)
-	w.Write(orders)
-}
+	}}}
 
 func fetchEbayOrders() []byte {
 	headers := map[string]string{"Authorization": fmt.Sprintf("Bearer %s", ebayToken)}
---
+++ backend/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Orders struct {
+	Orders []Order `json:"orders"`
+}
+
+type Order struct {
+	OrderItems      []OrderItem     `json:"orderItems"`
+	OrderDate       string          `json:"orderDate"`
+	OrderTotal      string          `json:"orderTotal"`
+	ShipByDate      string          `json:"shipByDate"`
+	ShippingAddress ShippingAddress `json:"shippingAddress"`
+	Platform        string          `json:"platform"`
+}
+
+type OrderItem struct {
+	ItemName  string `json:"itemName"`
+	ImageUrl  string `json:"imageUrl"`
+	ItemPrice string `json:"itemPrice"`
+}
+
+type ShippingAddress struct {
+	Name        string `json:"name"`
+	AddressLine string `json:"addressLine"`
+	City        string `json:"city"`
+	PostCode    string `json:"postCode"`
+	Country     string `json:"country"`
+}
+
+func main() {
+	http.HandleFunc("/get-orders", GetOrders)
+	log.Fatal(http.ListenAndServe(":3001", nil))
+}
+
+func GetOrders(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("content-type", "application/json")
+	//ebayOrders := fetchEbayOrders()
+	//ebayItemImage := fetchEbayItemImage("273258129797")xa
+
+	fmt.Println("MOCKED ORDERS: ", mockedEbayOrders)
+	orders, _ := json.Marshal(mockedEbayOrders)
+	w.Write(orders)
+}
